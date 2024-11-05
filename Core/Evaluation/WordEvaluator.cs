0cf9744fab68ca4ed2f0244d376fe37b8c78c67c
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2024-11-05 19:54:05 +0100

    First impl. Word file evaluation

---
+++ Core/Evaluation/WordEvaluator.cs
@@ -0,0 +1,11 @@
+﻿using Core.FileHandling;
+using Core.Questions;
+
+namespace Core.Evaluation; 
+
+public class WordEvaluator : IEvaluator<IQuestion<WordFile>, WordFile> {
+
+    public List<IQuestion<WordFile>> Questions { get; } = new();
+    public List<IEnumerable<WordFile>> Files { get; } = new();
+
+}
---
+++ Core/FileHandling/WordFile.cs
@@ -0,0 +1,31 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Core.FileHandling; 
+
+public class WordFile : IFile {
+    public string FileFormat { get; } = "docx";
+
+    private readonly WordprocessingDocument _doc;
+    public MainDocumentPart MainDoc { get; }
+    public CoreFilePropertiesPart CoreProps { get; }
+
+
+    public IEnumerable<Style> Styles {
+        get {
+            if (MainDoc.StylesWithEffectsPart is { } s)
+                return s.Styles?.Elements<Style>() ?? throw new ArgumentException();
+            return MainDoc.StyleDefinitionsPart?.Styles?.Elements<Style>() ?? throw new ArgumentException();
+        }
+    }
+
+    public WordFile(Stream file) {
+        _doc = WordprocessingDocument.Open(file, false);
+        MainDoc = _doc.MainDocumentPart ?? throw new ArgumentException();
+        CoreProps = _doc.CoreFilePropertiesPart ?? throw new ArgumentException();
+    }
+
+    public void Dispose() {
+        _doc.Dispose();
+    }
+}
