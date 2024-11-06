ad0414be325fc8a4f6a20e2d558cd02c72b83b98
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2024-11-06 20:33:10 +0100

    Small test fix

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,17 +7,14 @@ using Color = System.Drawing.Color;
 namespace Tests;
 
 public class UtilsTests {
-
+         
     [TestCase("FFFFFF", 255, 255, 255)]
     [TestCase("000000", 0, 0, 0)]
     [TestCase("FF0000", 255, 0, 0)]
     [TestCase("00FF00", 0, 255, 0)]
     [TestCase("0000FF", 0, 0, 255)]
     public void HexStringToRgb_TestCase(string hex, int r, int g, int b) {
-        var (resR, resG, resB) = hex.HexStringToRgb();
-        Assert.That(resR, Is.EqualTo(r));
-        Assert.That(resG, Is.EqualTo(g));
-        Assert.That(resB, Is.EqualTo(b));
+        Assert.That((r, g, b), Is.EqualTo(hex.HexStringToRgb()));
     }
 
 }
