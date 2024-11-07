6cde55827686052f06a8a4ab7cb88441734da384
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2024-11-07 17:02:33 +0100

    Custom exceptions to handle errors

---
+++ Core/Utils/Errors/Exceptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Utils.Errors;
+
+public class ApplicationException(string? msg = null, Exception? inner = null) : Exception(msg, inner);
+
+public class InvalidFileFormat : ApplicationException;
+public class FileError(Exception inner) : ApplicationException(null, inner);
