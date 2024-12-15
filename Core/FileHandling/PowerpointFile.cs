405f637197707104f044b650bf30550c26c01547
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2024-12-15 19:01:58 +0100

    Pictures retrieval from PowerpointFile.cs

--- Core/FileHandling/PowerpointFile.cs
+++ Core/FileHandling/PowerpointFile.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Utils.Errors;
 using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
 using ApplicationException = System.ApplicationException;
 
 namespace Core.FileHandling;
@@ -7,13 +8,26 @@ namespace Core.FileHandling;
 public class PowerpointFile : IFile {
 
     private readonly PresentationDocument _doc;
+    private readonly PresentationPart _mainDoc;
     
     public string Name { get; }
+
+    public IEnumerable<Picture> Pictures {
+        get {
+            try {
+                var slides = _mainDoc.Presentation.PresentationPart?.SlideParts ?? throw new InvalidFileFormat(Name);
+                return slides.SelectMany(s => s.Slide.Elements<Picture>());
+            } catch (Exception e) when (e is not ApplicationException) {
+                throw new FileError(Name, e);
+            }
+        }
+    }
 
     public PowerpointFile(string name, Stream file) {
         Name = name;
         try {
             _doc = PresentationDocument.Open(file, false);
+            _mainDoc = _doc.PresentationPart ?? throw new InvalidFileFormat(Name);
         } catch (Exception e) when (e is not ApplicationException) {
             throw new FileError(Name, e);
         }
