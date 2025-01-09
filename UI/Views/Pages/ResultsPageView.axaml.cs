b40cca529c872a6a553a95b29acdcb9162e65850
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2025-01-09 19:24:11 +0100

    FileResult control refactor

--- UI/Views/Pages/ResultsPageView.axaml.cs
+++ UI/Views/Pages/ResultsPageView.axaml.cs
@@ -1,28 +1,9 @@
-﻿using System.Linq;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.LogicalTree;
-using UI.ViewModels.Questions;
+﻿using Avalonia.Controls;
 
 namespace UI.Views.Pages;
 
 public partial class ResultsPageView : UserControl {
-    
     public ResultsPageView() {
         InitializeComponent();
     }
-
-    public void OnExpanding(object sender, CancelRoutedEventArgs args) {
-        var exp = (sender as Expander)!;
-        var vm = (exp.DataContext as FileResultViewModel)!;
-        if (!vm.IsExpandable)
-            args.Cancel = true;
-    }
-
-    public void OnSelectionChanged(object sender, SelectionChangedEventArgs args) {
-        var tree = (sender as TreeView)!;
-        foreach (var item in tree.GetLogicalDescendants().OfType<TreeViewItem>().Where(i => i.IsSelected))
-            item.IsSelected = false;
-    }
-    
 }
